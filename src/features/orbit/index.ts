f5b85f1b24144709e3030b85facb1f1647843de3
ohager <oliver.hager@dextra-sw.com>
2020-06-05 01:53:44 -0300

    fix: renamed feature folder to unignore it

---
+++ src/features/orbit/index.ts
@@ -0,0 +1,28 @@
+// @ts-ignore
+import IpfsClient from 'ipfs-http-client'
+// @ts-ignore
+import OrbitDB from 'orbit-db'
+import { logger } from '../logger'
+
+export class OrbitDbService {
+  private orbitdb: any = null
+  constructor(private ipfs: any) {}
+  async start() {
+    logger.info('Connecting to IPFS daemon')
+    this.ipfs = IpfsClient()
+    logger.info('Starting OrbitDb...')
+    this.orbitdb = await OrbitDB.createInstance(this.ipfs)
+    // TODO: define proper database type
+    const db = await this.orbitdb.log('hellos')
+    logger.info('Orbit Database instantiated', db.identity.toJSON())
+  }
+
+  async stop() {
+    logger.info('Stopping OrbitDB')
+    if (this.orbitdb) {
+      await this.orbitdb.stop()
+    }
+  }
+}
+
+export const orbitDbService = new OrbitDbService(IpfsClient())
