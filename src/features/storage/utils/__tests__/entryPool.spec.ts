789376def0443f252380f715a1af671459d9e6e2
ohager <oliver.hager@dextra-sw.com>
2020-07-08 20:33:05 -0300

    feat: added unit test for deduping

--- src/features/storage/utils/__tests__/entryPool.spec.ts
+++ src/features/storage/utils/__tests__/entryPool.spec.ts
@@ -22,6 +22,26 @@ describe('EntryPool', () => {
     expect(pool.entries).toEqual([6])
   })
 
+  it('should discard doubled entries with deduplication active', () => {
+    const callback = (_: number[]): Promise<void> => Promise.resolve()
+
+    const action = jest.fn(callback)
+
+    const pool = new EntryPool<number>()
+
+    pool.initialize({
+      timeout: 5 * Seconds,
+      limit: 5,
+      action,
+      dedupeFn: (entry, otherEntry) => entry === otherEntry,
+    })
+
+    const items = [1, 1, 1, 2, 2, 3, 4, 5]
+    items.forEach((n) => pool.addEntry(n))
+
+    expect(action).toBeCalledWith([1, 2, 3, 4, 5])
+  })
+
   it('should trigger action after timeout', async () => {
     const callback = (_: number[]): Promise<void> => Promise.resolve()
 
