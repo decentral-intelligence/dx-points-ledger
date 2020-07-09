545a668232a9a4fd40da47a5bdf9c339e3419db4
ohager <oliver.hager@dextra-sw.com>
2020-07-09 13:11:27 -0300

    feat: added transaction validation function and tests

--- src/features/storage/utils/__tests__/verifyTransaction.spec.ts
+++ src/features/storage/utils/__tests__/verifyTransaction.spec.ts
@@ -1,9 +1,17 @@
 // @ts-ignore
 import stableStringify from 'json-stable-stringify'
-import { generateKeyPairSync, verify, sign, createSign, createVerify, getCurves } from 'crypto'
+import { createSign, generateKeyPairSync } from 'crypto'
+import { TransactionData } from '../../models/TransactionData'
+import { verifyTransaction } from '../verifyTransaction'
+import { AccountData } from '../../models/Account'
+import { AccountRole } from '../../types/AccountRole'
 
-describe('verifySignature', () => {
-  it('should correctly verify a valid signature', () => {
+describe('verifyTransaction', () => {
+  let privKey: string
+  let pubKey: string
+  const TestSecret = 'TestSecret'
+
+  beforeAll(() => {
     const { publicKey, privateKey } = generateKeyPairSync('ec', {
       namedCurve: 'sect571r1',
       publicKeyEncoding: {
@@ -14,32 +22,85 @@ describe('verifySignature', () => {
         type: 'pkcs8',
         format: 'pem',
         cipher: 'aes-256-cbc',
-        passphrase: 'top secret',
+        passphrase: TestSecret,
       },
     })
 
-    const data = {
-      foo: 'bar',
+    privKey = privateKey
+    pubKey = publicKey
+  })
+
+  it('should correctly verify a valid transaction', () => {
+    const sender: AccountData = {
+      publicKey: pubKey,
+      _id: 'senderId',
+      alias: 'Test Account',
+      balance: 1000,
+      isActive: true,
+      role: AccountRole.Admin,
     }
-    const message = Buffer.from(stableStringify(data))
+    const transactionData: any = {
+      message: 'Some message',
+      sender: 'senderId',
+      recipient: 'recipientId',
+      amount: 1000,
+    }
+    const message = Buffer.from(stableStringify(transactionData))
     const signer = createSign('sha512')
     signer.update(message)
     signer.end()
 
     const signature = signer.sign({
-      key: privateKey,
-      passphrase: 'top secret',
+      key: privKey,
+      passphrase: TestSecret,
     })
-    const serializedSignature = signature.toString('base64')
 
-    const verifier = createVerify('sha512')
-    verifier.update(message)
-    verifier.end()
+    const signedTransaction: TransactionData = {
+      ...transactionData,
+      hash: 'somehash',
+      timestamp: 1000,
+      signature,
+    }
 
-    let recreatedSignature = Buffer.from(serializedSignature, 'base64')
+    expect(() => {
+      verifyTransaction(signedTransaction, sender)
+    }).not.toThrow()
+  })
 
-    const verified = verifier.verify(publicKey, recreatedSignature)
+  it('should correctly throw exception for an invalid transaction', () => {
+    const sender: AccountData = {
+      publicKey: 'Some invalid pubkey',
+      _id: 'senderId',
+      alias: 'Test Account',
+      balance: 1000,
+      isActive: true,
+      role: AccountRole.Admin,
+    }
+    const transactionData: any = {
+      message: 'Some message',
+      sender: 'senderId',
+      recipient: 'recipientId',
+      amount: 1000,
+    }
+    const message = Buffer.from(stableStringify(transactionData))
+    const signer = createSign('sha512')
+    signer.update(message)
+    signer.end()
 
-    expect(verified).toBeTruthy()
+    const signature = signer.sign({
+      key: privKey,
+      passphrase: TestSecret,
+    })
+
+    const signedTransaction: TransactionData = {
+      ...transactionData,
+      hash: 'somehash',
+      timestamp: 1000,
+      signature,
+    }
+
+    expect(() => {
+      verifyTransaction(signedTransaction, sender)
+    }).toThrow()
   })
 })
