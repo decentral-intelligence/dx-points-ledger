c5dde5cf64605990b1163caa6b376b347fa58666
ohager <oliver.hager@dextra-sw.com>
2021-07-29 18:22:18 -0300

    chore: use bash script

--- scripts/start-docker-instance.sh
+++ scripts/start-docker-instance.sh
@@ -4,5 +4,6 @@ ENV_FILE=$(dirname "$0")/docker.env
 
 docker run \
 --env-file "$ENV_FILE" \
--p 3001:3001 -p 4001:4001 -p 40011:4011 -p 40012:4012  \
+-p 3001:3001 -p 4001:4001 -p 4011:4011 -p 4012:4012  \
+-v dxpvol:/app \
 dx-points-backbone
